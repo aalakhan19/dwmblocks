e7745ffc34d45ac63f21f269f67adfa1f30bec1d
Aala Khan <aalakhan19@googlemail.com>
2022-10-02 18:40:11 +0200

    init

---
+++ blocks.def.h
@@ -0,0 +1,12 @@
+// Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal + 34*/
+	{"", "echo VOL: $(pactl list sinks | grep -iq 'stumm: ja' && echo muted || (pactl list sinks | grep Lautstärke | head -n1 | awk '{print $5}'))", 0, 10},
+	{"", "echo BAT: $(upower -i $(upower -e | grep BAT) | grep percentage | awk '{print $2}')", 60, 0},
+	{"", "date +'%d.%m.%y %H:%M'", 60, 0},
+
+};
+
+// sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
