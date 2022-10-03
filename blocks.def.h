de68491128de90d88d0ab6e50367d27f58d640ae
Aala Khan <aalakhan19@googlemail.com>
2022-10-03 22:42:13 +0200

    Update blocks.def.h

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,7 @@
 // Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal + 34*/
-	{"", "echo VOL: $(pactl list sinks | grep -iq 'stumm: ja' && echo muted || (pactl list sinks | grep Lautstärke | head -n1 | awk '{print $5}'))", 0, 10},
+	{"", "echo VOL: $(pactl list sinks | grep -iq 'muted: yes' && echo muted || (pactl list sinks | grep -i volume | head -n1 | awk '{print $5}'))", 0, 10},
 	{"", "echo BAT: $(upower -i $(upower -e | grep BAT) | grep percentage | awk '{print $2}')", 60, 0},
 	{"", "date +'%d.%m.%y %H:%M'", 60, 0},
 
